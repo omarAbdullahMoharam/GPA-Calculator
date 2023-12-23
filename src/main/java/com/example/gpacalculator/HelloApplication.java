1b244442b472e10a02b92ee5b1b73376d3f24d3b
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2023-12-23 18:53:24 +0200

    initial Commit

---
+++ src/main/java/com/example/gpacalculator/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.gpacalculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        showFirstScene(stage);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+    private void showFirstScene(Stage stage) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 873, 540);
+        stage.setTitle("Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
