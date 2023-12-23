855cf3975ee1a5a6ba906202269cff2ff95e6b39
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2023-12-24 00:06:58 +0200

    update Enroll Page+ Suggest Page

--- src/main/java/com/example/gpacalculator/HelloApplication.java
+++ src/main/java/com/example/gpacalculator/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 //        showGPAScene(stage);
-//        showSuggestScene(stage);
+        showSuggestScene(stage);
 //        showEnrollScene(stage);
 //          showSuggestScene(stage);
     }
