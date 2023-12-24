19a49360f7e340f7e10900effa4b5b74a0a02a88
Omar Abdullah <omarmoharam790@gmail.com>
2023-12-24 02:10:16 +0200

    test Action buttons

--- src/main/java/com/example/gpacalculator/HelloApplication.java
+++ src/main/java/com/example/gpacalculator/HelloApplication.java
@@ -1,7 +1,10 @@
 package com.example.gpacalculator;
-
 import javafx.application.Application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,13 +13,21 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        showEnrollScene(stage);
-    }
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
 
-    public static void main(String[] args) {
-        launch();
-    }
-    private void showFirstScene(Stage stage) throws IOException{
+        Stage newStage = new Stage();
+        newStage.setTitle("Login");
+        newStage.setScene(new Scene(root));
+        newStage.setOnCloseRequest(e -> ((Stage) root.getScene().getWindow()).close());
+        newStage.show();
+
+        showFirstScene(stage);
+        showEnrollScene(stage);
+        showUpdateScene(stage);
+        showCalculatorScene(stage);
+
+    }   private void showFirstScene(Stage stage) throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 873, 540);
         stage.setTitle("Welcome");
@@ -29,6 +40,49 @@ public class HelloApplication extends Application {
         stage.setTitle("Welcome");
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    void Update(ActionEvent event) throws IOException {
+        ///////// API Calling Place
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Update.fxml"));
+        Parent root = loader.load();
+        Stage newStage = new Stage();
+        newStage.setTitle("Login");
+        newStage.setScene(new Scene(root));
+        newStage.setOnCloseRequest(e -> ((Stage) root.getScene().getWindow()).close());
+        newStage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+    private void showUpdateScene(Stage stage) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Update.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 873, 540);
+        stage.setTitle("Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+    private void showCalculatorScene(Stage stage) throws IOException{
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("GPACalculator.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 873, 540);
+        stage.setTitle("Welcome");
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void Calculate(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GPACalculator.fxml"));
+        Parent root = loader.load();
+        Stage newStage = new Stage();
+        newStage.setTitle("Login");
+        newStage.setScene(new Scene(root));
+        newStage.setOnCloseRequest(e -> ((Stage) root.getScene().getWindow()).close());
+        newStage.show();
+    }
+    @FXML
+    void Help(ActionEvent event) {
+
     }
 
 }
